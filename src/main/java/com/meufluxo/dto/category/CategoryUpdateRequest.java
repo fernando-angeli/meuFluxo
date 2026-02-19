07ddca24238a05af4f3ff94248e73cb2ca4eb414
luiz-angeli <ext.luiz.angeli@crmbonus.com>
2026-02-19 15:41:02 -0300

    fix: refatoração de categorias

--- src/main/java/com/meufluxo/dto/category/CategoryUpdateRequest.java
+++ src/main/java/com/meufluxo/dto/category/CategoryUpdateRequest.java
@@ -7,7 +7,9 @@ public record CategoryUpdateRequest(
 
         @NotBlank(message = "O nome da categoria é obrigatório")
         @Size(min = 3, message = "O nome da categoria deve conter no mínimo 3 caracteres")
-        String name
+        String name,
+
+        Boolean active
 
 ) {
 }
