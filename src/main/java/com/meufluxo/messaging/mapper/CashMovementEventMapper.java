1acfcd1d30f3e635a4a1d6d4a111ec78e4a822d5
fernando-angeli <fernando_angeli@hotmail.com>
2026-03-08 16:57:38 -0300

    feat: implementa eventos kafka para movimentacoes

--- src/main/java/com/meufluxo/messaging/mapper/CashMovementEventMapper.java
+++ src/main/java/com/meufluxo/messaging/mapper/CashMovementEventMapper.java
@@ -1,4 +1,21 @@
 package com.meufluxo.messaging.mapper;
 
-public class CashMovementEventMapper {
+import com.meufluxo.messaging.events.CashMovementEvent;
+import com.meufluxo.model.CashMovement;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
+public interface CashMovementEventMapper {
+
+    @Mapping(target = "eventId", ignore = true)
+    @Mapping(target = "eventType", ignore = true)
+    @Mapping(target = "occurredAt", ignore = true)
+    @Mapping(target = "movementId", source = "id")
+    @Mapping(target = "accountId", source = "account.id")
+    @Mapping(target = "categoryId", source = "subCategory.category.id")
+    @Mapping(target = "movementType", expression = "java(movement.getMovementType().name())")
+    @Mapping(target = "movementDate", expression = "java(movement.getOccurredAt() != null ? movement.getOccurredAt().atStartOfDay() : null)")
+    CashMovementEvent toEvent(CashMovement movement);
 }
