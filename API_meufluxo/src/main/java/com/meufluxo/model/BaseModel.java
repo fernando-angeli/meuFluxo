7bb111536d14e2c925f2889ce5f8f769edd5dc32
fernando-angeli <fernando_angeli@hotmail.com>
2026-03-09 22:48:45 -0300

    feat: adicionar auditoria de usuario com spring data auditing

--- API_meufluxo/src/main/java/com/meufluxo/model/BaseModel.java
+++ API_meufluxo/src/main/java/com/meufluxo/model/BaseModel.java
@@ -1,38 +1,47 @@
 package com.meufluxo.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseModel {
 
+    @CreatedDate
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
     @Column(nullable = false)
     private LocalDateTime updatedAt;
+
+    @CreatedBy
+    @Column(name = "created_by_user_id", updatable = false)
+    private Long createdByUserId;
+
+    @LastModifiedBy
+    @Column(name = "updated_by_user_id")
+    private Long updatedByUserId;
 
     @Column(nullable = false)
     private boolean active = true;
 
     @PrePersist
     protected void onCreate() {
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
         this.active = true;
-    }
-
-    @PreUpdate
-    protected void onUpdate() {
-        this.updatedAt = LocalDateTime.now();
     }
 
 }
